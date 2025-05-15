bfabc9b104b6d2e228840bebb0189451969150ac
Justin Sunseri <jmsunseri@gmail.com>
2025-05-15 10:53:41 +0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -10,21 +10,12 @@ const config = {
 	preprocess: [vitePreprocess()],
 	
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '200.html',
-			precompress: false,
-			strict: true
+			fallback: '404.html'
 		}),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/blueretrowebcfg' : '',
-        }
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 export default config;
